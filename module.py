0a3ac55b22d83ee74423f8b043c98a83a21a9fd6
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2021-06-02 16:23:26 +0200

    changed variable names

--- module.py
+++ module.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 kernel = np.ones((3, 3), np.uint8)
-def mask(img):
+def mask(image):
     # define region of interest
-    roi = img[50:350, 50:350]
-    cv2.rectangle(img, (50, 50), (350, 350), (0, 255, 0), 0)
-    hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    green_frame = image[50:350, 50:350]
+    cv2.rectangle(image, (50, 50), (350, 350), (0, 255, 0), 0)
+    hsv = cv2.cvtColor(green_frame, cv2.COLOR_BGR2HSV)
     # define range of skin color in HSV
     lower_skin = np.array([0, 20, 70], dtype=np.uint8)
     upper_skin = np.array([20, 255, 255], dtype=np.uint8)
@@ -16,5 +16,5 @@ def mask(img):
     mask = cv2.erode(mask, kernel, iterations=1)
     # blur the image
     mask = cv2.GaussianBlur(mask, (5, 5), 100)
-    img = cv2.flip(img, 1)
+    image = cv2.flip(image, 1)
     return mask
